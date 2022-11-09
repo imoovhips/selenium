20bb46f3a3d924de64cc2851aec12606c52b05ac
imoovhips <antipenkomv@bk.ru>
2022-11-09 11:18:31 +0700

    search in xpath

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/Users/imoovhips/Desktop/chromedriver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.delivery-club.ru/moscow");
 
-        WebElement use = driver.findElement(By.xpath("//button[@class='header__search-button']"));
-        //
-        use.click();
+        //h3[contains(@class, '')][contains(@class, '')] - уточнение элемента (содержит что-то)
+        //h3[contains(@class, '')[not(contains(@class,''))] - уточнение (содержит что-то и не содержит чего-то)
+
+        //div[@id='']/h3 - нашли элемент и взяли его наследника (тот элемент, который под ним)
+        //div[@id='']//h3 - пропустили все элементы сверху до нужного если они есть
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://uitestingplayground.com/");
+
+        WebElement use = driver.findElement(By.xpath("//div[@class='row']//h1"));
+        System.out.println(use + "элемент найден");
+        driver.quit();
 
     }
 }
